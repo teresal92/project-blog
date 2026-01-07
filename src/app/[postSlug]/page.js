1fc35438607e366556711e0bdad48adb22b372ed
Teresa Lew <teresalew92@gmail.com>
2026-01-06 16:16:14 -0800

    ex-4 solution

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 import BlogHero from "@/components/BlogHero";
+import CodeSnippet from "@/components/CodeSnippet";
 
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
@@ -28,7 +29,12 @@ async function BlogPost({ params }) {
         <>
           <BlogHero title={title} publishedOn={new Date(publishedOn)} />
           <div className={styles.page}>
-            <MDXRemote source={content} />
+            <MDXRemote
+              source={content}
+              components={{
+                pre: CodeSnippet,
+              }}
+            />
           </div>
         </>
       ) : (
